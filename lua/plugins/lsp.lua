834501508f47b1221016f9e2a64cc4fda5e104f0
nyx-ware <aldrnidheimr@gmail.com>
2025-11-04 20:08:09 +0000

    updated lsp to the new mason version instead of just downgrading

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,8 @@
 return {
-    "williamboman/mason-lspconfig.nvim",
-    branch = "v1.x",
+    "mason-org/mason-lspconfig.nvim",
     dependencies = {
         "neovim/nvim-lspconfig",
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
@@ -33,27 +32,6 @@ return {
             }, {
                 { name = "buffer" }
             })
-        })
-
-        local capabilities  = require("cmp_nvim_lsp").default_capabilities()
-        require("mason-lspconfig").setup_handlers({
-            function (server_name)
-                if server_name == "rust_analyzer" then
-                    return
-                end
-
-                if server_name == "clangd" then
-                    require("lspconfig").clangd.setup({
-                        cmd = { "clangd", "--header-insertion=never" }
-                    })
-
-                    return
-                end
-
-                require("lspconfig")[server_name].setup({
-                    capabilities = capabilities 
-                })
-            end
         })
     end
 }
